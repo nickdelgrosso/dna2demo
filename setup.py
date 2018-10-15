569134eb0f5df46c04390def0d825b3e0c744620
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2018-10-15 23:12:57 +0200

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dna',
+    version='',
+    py_modules=['dna'],
+    url='',
+    license='',
+    author='Nicholas Del Grosso',
+    author_email='',
+    description='',
+)
