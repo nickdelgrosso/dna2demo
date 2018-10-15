4cc0c7ac8406aeab273c024587bae684c588ca4b
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2018-10-15 22:33:12 +0200

    made pytest functions

--- dna.py
+++ dna.py
@@ -11,24 +11,21 @@ def complement_sequence(seq):
 
     Returns:
         -complement (str): the reverse complement of the sequence.
-
-    Examples:
-    >>> complement_sequence("GCA")
-    'TGC'
-
-    >>> complement_sequence('GGG')
-    'CCC'
-
-    >>> complement_sequence('ATGCAGT')
-    'ACTGCAT'
-
-    >>> complement_sequence('atc')
-    'GAT'
-
     """
     return ''.join(complement_nucleotides[nt] for nt in seq[::-1].upper())
 
 
+def test_complement_seq():
+    assert complement_sequence("GCA") == 'TGC'
+
+def test_complement_seq2():
+    assert complement_sequence("GGG") == 'CCC'
+
+def test_complement_seq3():
+    assert complement_sequence("ATGCAGT") == 'ACTGCAT'
+
+def test_complement_upper():
+    assert complement_sequence("atc") == 'GAT'
 
 
 if __name__ == '__main__':
